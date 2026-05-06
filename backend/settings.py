b116ca85537594fadef31858b09c121e068ec999
ingura088 <ingurareagan088@gmail.com>
2026-05-06 16:49:40 +0300

    Fix backend for production

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,14 @@ SECRET_KEY = 'django-insecure-6=tk%g^fh2#_s$zhn41%mbeq2ud1)@jp*jmq^*c*np56nr32il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
-
+ALLOWED_HOSTS = [
+    "e-voting-hn92.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://e-voting-hn92.onrender.com"
+]
 # Application definition
 
 INSTALLED_APPS = [ 
